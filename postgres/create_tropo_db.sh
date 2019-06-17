e2384901afa36404e6c3a31a6c17efcc853c0295
Calvin McLean <calvinmclean@email.arizona.edu>
2019-06-17 15:55:16 -0700

    Allow populating Troposphere database from dump

--- postgres/create_tropo_db.sh
+++ postgres/create_tropo_db.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
-POSTGRES="psql --username ${POSTGRES_USER} ${POSTGRES_DB}"
+POSTGRES="psql --username ${POSTGRES_USER}"
 
 echo "Creating database: ${TROPO_DB_NAME}"
 
-$POSTGRES <<EOSQL
+$POSTGRES ${POSTGRES_DB} <<EOSQL
 CREATE DATABASE ${TROPO_DB_NAME} OWNER ${POSTGRES_USER};
 EOSQL
+
+if [ -e /docker-entrypoint-initdb.d/tropo*.sql.dump ]; then
+  echo "Loading Troposphere database dump"
+  $POSTGRES ${TROPO_DB_NAME} -f /docker-entrypoint-initdb.d/tropo*.sql.dump
+fi
