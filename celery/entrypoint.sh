190c28a04fa82dcdf6b04ee5d70ab58ebd0dcb4b
Calvin McLean <calvinmclean@email.arizona.edu>
2019-08-21 13:51:23 -0700

    Add redis container

--- celery/entrypoint.sh
+++ celery/entrypoint.sh
@@ -10,10 +10,8 @@ else
   groupmod -g $user_id user
 fi
 
-chown -R $user_id:$user_id /opt/dev/atmosphere
 if [[ $env_type = "dev" ]]
 then
-  sed -i "s/^bind 127.0.0.1 ::1$/bind 127.0.0.1/" /etc/redis/redis.conf
   sed -i "s/^CELERYD_USER=\"www-data\"$/CELERYD_USER=\"user\"/" /etc/init.d/celeryd
   sed -i "s/^CELERYD_GROUP=\"www-data\"$/CELERYD_GROUP=\"$user_id\"/" /etc/init.d/celeryd
   sed -i "s/^CELERY_USER=\"www-data\"$/CELERY_USER=\"user\"/" /etc/init.d/celerybeat
@@ -23,7 +21,7 @@ else
   chown -R www-data:www-data /opt/dev/atmosphere
 fi
 
-service redis-server start
+
 service celerybeat start
 service celeryd start
 
